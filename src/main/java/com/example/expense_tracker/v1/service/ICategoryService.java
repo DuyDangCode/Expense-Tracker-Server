27258a04195f37b7d64cbbfa8c2bb8dda20318c9
Thành Duy <21520774@gm.uit.edu.vn>
2025-07-17 18:29:29 +0700

    #6 create category service interface

---
+++ src/main/java/com/example/expense_tracker/v1/service/ICategoryService.java
@@ -0,0 +1,18 @@
+package com.example.expense_tracker.v1.service;
+
+import com.example.expense_tracker.v1.dto.CreateCategoryDto;
+import com.example.expense_tracker.v1.dto.UpdateCategoryDto;
+import com.example.expense_tracker.v1.model.CategoryModel;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ICategoryService {
+    CategoryModel create(CreateCategoryDto categoryInput);
+
+    CategoryModel getInfoById(long categoryId) throws Exception;
+
+    List<CategoryModel> getAll(long userId);
+
+    boolean update(UpdateCategoryDto categoryUpdateDto, long categoryId);
+}
