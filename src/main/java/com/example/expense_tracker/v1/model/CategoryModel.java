fd8f551606d6a777d07a9af15a6634e32b941d9a
Thành Duy <21520774@gm.uit.edu.vn>
2025-07-17 18:18:04 +0700

    #6 create category model

--- src/main/java/com/example/expense_tracker/v1/model/CategoryModel.java
+++ src/main/java/com/example/expense_tracker/v1/model/CategoryModel.java
@@ -1,24 +1,26 @@
 package com.example.expense_tracker.v1.model;
 
 
-import com.example.expense_tracker.v1.dto.CategoryCreateDto;
+import com.example.expense_tracker.v1.dto.CreateCategoryDto;
+import com.example.expense_tracker.v1.dto.UpdateCategoryDto;
 import jakarta.persistence.*;
 
 @Entity
 @Table(name = "Categories")
-public class CategoryModel {
-
-    @Id
-    private long id;
-
+public class CategoryModel extends BaseModel {
     @Column(name = "UserId", nullable = false)
     private long userId;
 
     @Column(name = "Name")
     private String name;
 
-    public CategoryModel(CategoryCreateDto categoryCreateDto) {
+    public CategoryModel(CreateCategoryDto categoryCreateDto) {
         this.userId = categoryCreateDto.getUserId();
         this.name = categoryCreateDto.getName();
+    }
+
+    public CategoryModel updateName(UpdateCategoryDto newCategory) {
+        this.name = newCategory.getName();
+        return this;
     }
 }
