cbfdc53965ab0ab66abbd50491341074eafd9b7e
Thành Duy <21520774@gm.uit.edu.vn>
2025-07-17 18:18:22 +0700

    #6 add some method for category service

--- src/main/java/com/example/expense_tracker/v1/service/CategoryService.java
+++ src/main/java/com/example/expense_tracker/v1/service/CategoryService.java
@@ -1,37 +1,55 @@
 package com.example.expense_tracker.v1.service;
 
-import com.example.expense_tracker.v1.dto.CategoryCreateDto;
-import com.example.expense_tracker.v1.dto.CategoryUpdateDto;
+import com.example.expense_tracker.v1.dto.CreateCategoryDto;
+import com.example.expense_tracker.v1.dto.UpdateCategoryDto;
 import com.example.expense_tracker.v1.model.CategoryModel;
 import com.example.expense_tracker.v1.repo.CategoryRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.security.InvalidParameterException;
+import java.util.List;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-public class CategoryService {
+public class CategoryService implements ICategoryService {
+
     private final CategoryRepo categoryRepo;
 
-    public CategoryModel create(CategoryCreateDto categoryInput) {
+    public CategoryModel create(CreateCategoryDto categoryInput) {
         CategoryModel newCategory = categoryRepo.save(new CategoryModel(categoryInput));
         return newCategory;
     }
 
-    public CategoryModel getInfoById(long id) throws Exception {
-        Optional<CategoryModel> foundCategory = categoryRepo.findById(id);
+    public CategoryModel getInfoById(long categoryId) throws Exception {
+        Optional<CategoryModel> foundCategory = categoryRepo.findById(categoryId);
         if (foundCategory.isEmpty())
             throw new Exception();
         return foundCategory.get();
     }
 
-    public boolean update(CategoryUpdateDto categoryUpdateDto, long id) {
+    public List<CategoryModel> getAll(long userId) {
+        return categoryRepo.findAllByUserId(userId);
+    }
 
+    public boolean update(UpdateCategoryDto categoryUpdateDto, long categoryId) {
+        CategoryModel foundCategory = checkAndGetCategoryExists(categoryId);
+        categoryRepo.save(foundCategory.updateName(categoryUpdateDto));
         return true;
     }
 
-    public boolean delete() {
+    private CategoryModel checkAndGetCategoryExists(long categoryId) {
+        Optional<CategoryModel> foundCategory = categoryRepo.findById(categoryId);
+        if (foundCategory.isEmpty()) {
+            throw new InvalidParameterException("Not found category");
+        }
+        return foundCategory.get();
+    }
+
+    public boolean delete(long categoryId) {
+        CategoryModel foundCategory = checkAndGetCategoryExists(categoryId);
+        foundCategory.delete();
         return true;
     }
 
