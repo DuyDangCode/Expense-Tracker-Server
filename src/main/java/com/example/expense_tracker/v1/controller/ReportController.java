1b430dc320c662d567c102e0b924bb74f91826d0
Thành Duy <21520774@gm.uit.edu.vn>
2025-07-17 18:31:40 +0700

    #8 create basic report controller

---
+++ src/main/java/com/example/expense_tracker/v1/controller/ReportController.java
@@ -0,0 +1,28 @@
+package com.example.expense_tracker.v1.controller;
+
+
+import com.example.expense_tracker.v1.dto.ResponseSuccess;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/reports")
+public class ReportController {
+    @GetMapping("/summary")
+    public ResponseSuccess getSummary() {
+        return new ResponseSuccess(HttpStatus.OK);
+    }
+
+    @GetMapping("/categories")
+    public ResponseSuccess getByCategory() {
+        return new ResponseSuccess(HttpStatus.OK);
+    }
+
+    @GetMapping("/export-csv")
+    public ResponseSuccess exportCsv() {
+        return new ResponseSuccess(HttpStatus.OK);
+    }
+}
