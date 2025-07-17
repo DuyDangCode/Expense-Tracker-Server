f91251470e705e16b9c9b1ef9d1fbf40ea723c61
Thành Duy <21520774@gm.uit.edu.vn>
2025-07-17 18:19:05 +0700

    #4 update global exception handler

--- src/main/java/com/example/expense_tracker/v1/config/GlobalExceptionHandler.java
+++ src/main/java/com/example/expense_tracker/v1/config/GlobalExceptionHandler.java
@@ -1,7 +1,10 @@
 package com.example.expense_tracker.v1.config;
 
 import com.example.expense_tracker.v1.dto.ErrorResponse;
+import io.swagger.v3.oas.annotations.Hidden;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.context.request.WebRequest;
@@ -9,12 +12,15 @@ import org.springframework.web.context.request.WebRequest;
 import java.util.Date;
 
 @RestControllerAdvice
+@Hidden
 public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
+    @ResponseStatus()
+    //todo: change message to "something went wrong"
     public ErrorResponse handleException(Exception e, WebRequest request) {
         var newErrorResponse = ErrorResponse.builder()
                 .error(e.toString())
-                .status(500)
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
                 .timestamp(new Date())
                 .path(request.getDescription(false).replace("uri=", ""))
                 .message(e.getMessage())
