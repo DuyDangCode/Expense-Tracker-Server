d04cfe1a0ca0d269da0e58129ff5dc28f99a30ce
Thành Duy <21520774@gm.uit.edu.vn>
2025-07-17 18:30:00 +0700

    #3 update swagger config

---
+++ src/main/java/com/example/expense_tracker/v1/config/OpenApiConfig.java
@@ -0,0 +1,40 @@
+package com.example.expense_tracker.v1.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springdoc.core.properties.SpringDocConfigProperties;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.List;
+
+@Configuration
+@Profile({"dev", "test"})
+public class OpenApiConfig {
+    @Bean
+    public GroupedOpenApi publicApi(@Value("${openapi.service.api-docs}") String apiDoc, @Value("${openapi.service.package-to-scan}") String packageToScane) {
+        return GroupedOpenApi.builder()
+                .group(apiDoc)
+                .packagesToScan(packageToScane)
+                .build();
+    }
+
+    @Bean
+    public OpenAPI openAPI(
+            @Value("${openapi.service.title}") String title,
+            @Value("${openapi.service.version}") String version,
+            @Value("${openapi.service.server}") String serverUrl) {
+        return new OpenAPI()
+                .servers(List.of(new Server().url(serverUrl)))
+                .info(new Info().title(title)
+                        .description("API documents")
+                        .version(version)
+                        .license(new License().name("Apache 2.0").url("https://springdoc.org")));
+    }
+
+}
